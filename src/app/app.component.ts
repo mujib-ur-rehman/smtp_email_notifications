83aa91d3c852f8972b9ad8f7fb8306ce8bbdf486
mujib-ur-rehman <mujib.rehman@focusteck.com>
2021-11-26 15:21:21 +0500

    SMTP using elasticemail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'src/assets/smtp.js'
 declare let Email: any;
 
 @Component({
@@ -8,15 +9,19 @@ declare let Email: any;
 })
 export class AppComponent {
   title = 'smtp';
+  email: any = 'mujib.rehman@focusteck.com'
   myFunction() {
     Email.send({
       Host: 'smtp.elasticemail.com',
       Username: 'mujib.rehman@focusteck.com',
       Password: 'F910853DCAD40D589CE288D84F971B93F174',
-      To: 'raza.sajjad@focusteck.com',
+      To: this.email,
       From: 'mujib.rehman@focusteck.com',
       Subject: 'Subject',
-      Body: '<i>This is sent as a feedback from my resume page.</i> <br/> <b>Name: </b><br /> <b>Email: </b><br /> <b>Subject: </b><br /> <b>Message:</b> <br /><br><br> <b>~End of Message.~</b> '
-    }).then(err => console.warn(err))
+      Body: 'Body'
+    }).then(function () {
+      alert("mail sent successfully")
+    }
+    )
   }
 }
